6361aad6437b2bb68423cc895edeef06afc03fd6
憂鬱的烏龜 <20069428+mikukonai@users.noreply.github.com>
2019-03-11 16:30:44 +0800

    增加测试用例（fork）

---
+++ demo/aurora/testcase/fork.scm
@@ -0,0 +1,23 @@
+;; 测试fork
+(define test
+  (lambda (n)
+    (if (= n 3)
+        (begin
+        (display "从外部模块fork子进程")
+        (fork "./main.scm")
+        (display "直接根据代码fork子进程")
+        (fork (begin (display "子进程开始啦")
+                     (define foo
+                        (lambda (n)
+                          (if (= n 0)
+                              1
+                              (* n (foo (- n 1))))))
+                     ((lambda (res)
+                        (begin (display "子进程里计算阶乘的结果：")
+                               (display res)))
+                      (foo 10))))
+        (test (+ n 1)))
+        (if (= n 6)
+            (display "父进程执行完毕")
+            (test (+ n 1))))))
+(test 1)
