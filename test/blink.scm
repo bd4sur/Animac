07b22c248b3e22a8317e91c176d3f2949cd132a1
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2025-04-13 14:24:15 +0800

    【测试】更新blink测试用例

--- test/blink.scm
+++ test/blink.scm
@@ -1,4 +1,4 @@
-;; 用 Scheme 控制树莓派上的LED使其闪烁
+;; 用 Scheme 控制路由器上的LED使其闪烁
 
 (native File)
 
@@ -14,20 +14,15 @@
           ((= num01 1) "1")
           (else "1"))))
 
-;; 应当加入GPIO是否被exported的判断。但是现在因缺乏相关接口，无法做到这一点。
-(File.WriteString "/sys/class/gpio/unexport" "2" "w")
-(File.WriteString "/sys/class/gpio/export" "2" "w")
-(File.WriteString "/sys/class/gpio/gpio2/direction" "out" "w")
-
 (define counter 0)
 
 (define Blink
   (lambda () {
-      (File.WriteString "/sys/class/gpio/gpio2/value" (ToStr (% counter 2)) "w")
+      (File.writeStringSync "/sys/devices/platform/leds/leds/yellow:network/brightness" (ToStr (% counter 2)) "w")
       (display (ToStr (% counter 2)))
       (set! counter (+ counter 1))
       (if (= (% counter 2) 0)
-          (Delay 10)
+          (Delay 1000)
           (Delay 2000))
       (Blink)
   }))
